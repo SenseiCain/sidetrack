706a543a6369fab0e578096bd50282b8819592ca
Christian Cain <christian22cain@yahoo.com>
2020-02-12 09:44:15 -0600

    refactored post display as a helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,30 @@
 module PostsHelper
+    def display_post(post)
+
+        if post.comments.any?
+            comments = post.comments.map do |c|
+                "<li>#{ c.user.name } - #{ c.description }</li>"
+            end.join('')
+
+            comments_html = "
+                <li style='list-style-type: none'>
+                    <ul>
+                        #{ comments }   
+                    </ul>
+                </li>"
+        else
+            comments_html = nil
+        end
+
+        raw("
+            <p>
+                <ul>
+                    <li>Title: #{ post.title }</li>
+                    <li>Description: #{ post.description }</li>
+                    <li>Vote count: #{ post.votes.count }</li>
+                    #{ comments_html}
+                </ul>
+            </p>
+        ")
+    end
 end
