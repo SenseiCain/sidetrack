db214fae6e09797c9dc6ddf6cae4ab9258052e3a
Christian Cain <christian22cain@yahoo.com>
2020-02-11 18:40:13 -0600

    add create methods for upvote/downvote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,12 @@ class Vote < ApplicationRecord
     belongs_to :post
 
     validates :status, inclusion: { in: [ true, false ] }
+
+    def self.create_upvote(user, post)
+        create(status: true, user: user, post: post)
+    end
+
+    def self.create_downvote(user, post)
+        create(status: false, user: user, post: post)
+    end
 end
