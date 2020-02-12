b225f3db2d9dc18c2dae4b4512af75694a15ed7b
Christian Cain <christian22cain@yahoo.com>
2020-02-11 18:10:44 -0600

    coverts title to titlecase before saving

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,19 @@
 class Post < ApplicationRecord
+    before_save :make_post_title_case
+    
     belongs_to :user
 
     has_many :posts_categories
     has_many :categories, through: :posts_categories
-    
+
     has_many :comments
 
     validates :title, presence: true
     validates :description, presence: true
 
+    private
+
+    def make_post_title_case
+        self.title = self.title.titlecase
+    end
 end
