1cad60cccc49588705f595b299acafc2c095d44a
Christian Cain <christian22cain@yahoo.com>
2020-02-12 08:47:23 -0600

    created routes for post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:show, :new, :create, :edit, :update, :destroy]
+  get '/', to: 'posts#index', as: 'root'
 end
