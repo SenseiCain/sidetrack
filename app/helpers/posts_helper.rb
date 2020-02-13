368e0c9cb66493be122bbfd1c1da57b3fd6d3aec
Christian Cain <christian22cain@yahoo.com>
2020-02-13 14:41:45 -0600

    stubbed out display_post helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,59 @@
 module PostsHelper
-    
+    # REFERENCE ARTICLE - https://www.toptal.com/ruby-on-rails/rails-helper-bootstrap-carousel
+
+
+    def display_post_tag(post)
+        PostTag.new(self, post).html
+    end
+
+    class PostTag
+        def initialize(view, post)
+            @view, @post = view, post
+            @uid = SecureRandom.hex(6)
+        end
+
+        def html
+            # Joins components & renders parent div
+            content = safe_join([vote, main])
+            content_tag(:div, content, id: 'uid', class: 'row')
+        end
+
+        private
+
+        attr_accessor :view, :post, :uid
+        delegate :link_to, :content_tag, :image_tag, :raw, :safe_join, to: :view
+
+        def vote
+            content = count_tag
+            content_tag(:div, content, class: 'col-md-1 d-flex justify-content-center align-items-center bg-info m-0 p-0')
+        end
+        
+        def count_tag
+            # Upvote & Downvote
+            # Vote count
+            content_tag(:h5, @post.votes.count)
+        end
+
+        def main
+            content = safe_join([title_tag, categories_tag, info_tag])
+            content_tag(:div, content, class: 'col-md-11 border')
+        end
+
+        def title_tag
+            # Post title
+            content_tag(:h5, @post.title, class: 'my-0')
+        end
+
+        def categories_tag
+            # Post categories that link to their filtered index
+            content = @post.categories.map{|c| content_tag(:div, c.name, class: 'small px-1 py-0 mr-1 my-0 bg-info rounded')}
+            content_tag(:div, safe_join(content), class: 'd-flex')
+        end
+
+        def info_tag
+            # Comments - User - Created @
+            content = "#{@post.comments.count} Comments - #{@post.user.name} - #{@post.created_at}"
+            content_tag(:p, content, class: 'small my-0')
+        end
+    end
 end
