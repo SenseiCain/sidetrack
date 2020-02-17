64f6e8322a0378d8481cadfd5ab0da82f3365833
Christian Cain <christian22cain@yahoo.com>
2020-02-17 17:54:45 -0600

    created basic login & logout flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,20 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(email: user_params[:email])
+        return head(:forbidden) unless @user.authenticate(user_params[:password])
+        session[:user_id] = @user.id
+        redirect_to root_path
     end
 
     def destroy
+        session.delete :user_id
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :name, :password)
     end
 end
