e144720f1c40956c1aa60bb0dba81535f26a3258
Christian Cain <christian22cain@yahoo.com>
2020-02-18 20:47:05 -0600

    handled for create a comment errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,25 @@
 class CommentsController < ApplicationController
     def create
-        raise params.inspect
+        if @user = current_user
+            @comment = Comment.new(description: comment_params[:description])
+            @comment.user = @user
+            @post = Post.find_by(id: comment_params[:post_id])
+            @comment.post = @post
+
+            if @comment.save
+                redirect_to post_path(@post)
+            else
+                redirect_to post_path(@post)
+            end
+        else
+            session[:return_to] ||= request.referer
+            redirect_to login_path
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:description, :post_id)
     end
 end
