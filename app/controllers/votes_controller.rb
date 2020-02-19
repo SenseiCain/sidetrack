332ca6c1355becc3f98c79e4870cfae7dab037c1
Christian Cain <christian22cain@yahoo.com>
2020-02-18 22:36:32 -0600

    handled conditions for creating votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,16 @@
 class VotesController < ApplicationController
     def create
+        @vote = Vote.find_or_initialize_by(user_id: vote_params[:user_id], post_id: vote_params[:post_id])
+        
+        if @vote.new_record?
+            @vote.status = vote_params[:status]
+            @vote.save
+        elsif @vote.status != vote_params[:status]
+            @vote.status = vote_params[:status]
+            @vote.save
+        end
+
+        redirect_to root_path
     end
 
     private
