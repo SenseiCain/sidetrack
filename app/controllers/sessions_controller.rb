1038760448621cb97f9dc6a5da3ea24b25b3066f
Christian Cain <christian22cain@yahoo.com>
2020-02-19 15:19:22 -0600

    redesigned log in flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,19 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(email: user_params[:email])
-        return head(:forbidden) unless @user.authenticate(user_params[:password])
-        session[:user_id] = @user.id
+
+        if @user && @user.authenticate(user_params[:password])
+            session[:user_id] = @user.id
         
-        if session[:return_to]
-            redirect_to session.delete(:return_to)
+            if session[:return_to]
+                redirect_to session.delete(:return_to)
+            else
+                redirect_to root_path
+            end
         else
-            redirect_to root_path
+            redirect_to login_path
         end
+        
     end
 
     def destroy
