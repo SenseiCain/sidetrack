c04cfb9b4c30341b9d12f231061a020879954097
Christian Cain <christian22cain@yahoo.com>
2020-02-18 20:46:37 -0600

    added validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :post
+
+    validates :description, presence: true
+    validates :post, presence: true
+    validates :user, presence: true
 end
