728b0bec75b911d5f3eb3fd736d470813ccc77ad
Christian Cain <christian22cain@yahoo.com>
2020-02-18 21:08:28 -0600

    added validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,9 @@ class Vote < ApplicationRecord
     belongs_to :user
     belongs_to :post
 
-    validates :status, inclusion: { in: [ true, false ] }
+    validates :status, inclusion: { in: [ true, false ] }, uniqueness: { scope: [:user_id, :post_id] }
+    validates :user, presence: true
+    validates :post, presence: true
 
     def self.create_upvote(user, post)
         create(status: true, user: user, post: post)
