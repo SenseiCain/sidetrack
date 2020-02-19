00b1ed6198012af48ef3d2fbc06c2fce6723c130
Christian Cain <christian22cain@yahoo.com>
2020-02-18 22:38:44 -0600

    removed unnecessary methods

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,12 +5,4 @@ class Vote < ApplicationRecord
     validates :status, inclusion: { in: [ true, false ] }, uniqueness: { scope: [:user_id, :post_id] }
     validates :user, presence: true
     validates :post, presence: true
-
-    def self.create_upvote(user, post)
-        create(status: true, user: user, post: post)
-    end
-
-    def self.create_downvote(user, post)
-        create(status: false, user: user, post: post)
-    end
 end
