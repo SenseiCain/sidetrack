2e5f134c169d73fac1e42be3d55dfd44b402af8a
Christian Cain <christian22cain@yahoo.com>
2020-02-21 15:24:58 -0600

    finds user by name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,22 @@ class UsersController < ApplicationController
     before_action :set_categories, :set_current_user
 
     def show
+        # raise params.inspect
+        # byebug
+        if @reference_user = User.find_by(name: url_decode(params[:id]))
+
+        else
+            redirect_to root_path
+        end
     end
 
     def create
 
+    end
+
+    private
+
+    def url_decode(name)
+        name.gsub!('-', ' ')
     end
 end
