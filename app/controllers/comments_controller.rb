cfb9ed996c1947dc918c69540b48df8baf93ccd7
Christian Cain <christian22cain@yahoo.com>
2020-02-22 14:23:58 -0600

    added functionality to delete post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
+    before_action :set_categories, :set_current_user
+
     def create
-        if @current_user = current_user
+        if @current_user
             @comment = Comment.new(description: comment_params[:description])
             @comment.user = @current_user
             @post = Post.find_by(id: comment_params[:post_id])
@@ -14,6 +16,18 @@ class CommentsController < ApplicationController
         else
             session[:return_to] ||= request.referer
             redirect_to login_path
+        end
+    end
+
+    def destroy
+        @comment = Comment.find_by(id: params[:id])
+        @post = @comment.post
+
+        if @comment && @comment.user = @current_user
+            @comment.destroy
+            redirect_to post_path(@post)
+        else
+            redirect_to root_path
         end
     end
 
