42023b347262a81e4565c52ffc63db0cdad6060c
Christian Cain <christian22cain@yahoo.com>
2020-02-23 12:42:06 -0600

    added for multiple omni providers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_224740) do
+ActiveRecord::Schema.define(version: 2020_02_23_184006) do
+
+  create_table "authorizations", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
