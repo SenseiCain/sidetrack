8e4ebffb806273f8c6a9af77ad171f307964b9b9
Christian Cain <christian22cain@yahoo.com>
2020-02-25 16:46:16 -0600

    added images to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_184006) do
+ActiveRecord::Schema.define(version: 2020_02_25_221706) do
 
   create_table "authorizations", force: :cascade do |t|
     t.string "provider"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_02_23_184006) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "votes", force: :cascade do |t|
